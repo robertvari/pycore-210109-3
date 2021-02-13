b7a2c49e174754ca30e0d02de0d3d44348793e4e
Vári Róbert <mail.robertvari@gmail.com>
2021-02-13 14:56:29 +0100

    class attribute vs instance attribute

--- 02_attributes.py
+++ 02_attributes.py
@@ -1,12 +1,19 @@
 class Person:
+    name = "Robert"
+
     def __init__(self, name, phone, address):
         self.name = name
         self.phone = phone
         self.address = address
+        self._id = 123456
+
+    def __str__(self):
+        return f"{self.name}, {self.phone}, {self.address}"
 
 
 csaba = Person("CSaba", "0620 555 66 88", "Budapest")
 laci = Person("Laci", "0620 666 7788", "Debrecen")
 
-print(csaba.name, csaba.phone, csaba.address)
-print(laci.name, laci.phone, laci.address)
+print(Person.name)
+Person.name = "Kriszta"
+print(Person.name)
