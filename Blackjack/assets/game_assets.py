f95d85f6599c484d2ac5af021adea08e477941a2
Vári Róbert <mail.robertvari@gmail.com>
2021-02-13 16:44:26 +0100

    card class

--- Blackjack/assets/game_assets.py
+++ Blackjack/assets/game_assets.py
@@ -7,4 +7,22 @@ class Deck:
 
 
 class Card:
-    pass
+    def __init__(self, name, value):
+        self._name = name
+        self._value = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def value(self):
+        return self._value
+
+    def __str__(self):
+        return f"{self.name} {self.value}"
+
+
+if __name__ == '__main__':
+    card = Card("Spade", 8)
+    print(card)
